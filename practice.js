9f2c315a19331f0c09d1be03b3f9baab32093502
Daniel Evers <contact@danielevers.online>
2020-01-08 20:48:28 -0700

    minor update on problem 5 & fixed

--- practice.js
+++ practice.js
@@ -89,13 +89,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-function evenFinder(nums) {
-  return (nums % 2 === 0);
+function evenFinder(nums){
+  var evens = []
+  for(i=0;i<nums.length;i++){
+    if(nums[i]%2===0){
+      evens.push(nums[i]);
+    }
+  }
+  return evens
 }
-
-
-
-
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
